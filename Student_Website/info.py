61efc7c23a01c06c2d344371a7a44730fc818e62
Mohit Makawana <121477855+MakawanaMohit@users.noreply.github.com>
2024-05-31 20:36:39 +0530

    Update info.py

--- Student_Website/info.py
+++ Student_Website/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mohitdiplomaitproject36@gmail.com'
-EMAIL_HOST_PASSWORD = 'uceb jhvq ozum txwm'
+EMAIL_HOST_USER = 'your@email.com'
+EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
